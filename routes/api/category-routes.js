f3077bf1a3730559d5eab6c0e912901bc40b3aa6
Shaitan5723 <105050107+Shaitan5723@users.noreply.github.com>
2022-08-15 16:21:36 +1000

    updated routes
    inserted async routes for products and tags

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,10 +14,10 @@ router.get('/', async (req, res) => {
     return;
   }
   res.status(200).json(categoryData);
-} catch(err) {
-  res.status(500).json(err);
-}
-  });
+  } catch(err) {
+      res.status(500).json(err);
+  }
+});
 
 
 router.get('/:id', async (req, res) => {
@@ -32,7 +32,7 @@ router.get('/:id', async (req, res) => {
   }
   res.status(200).json(categoryData);
   } catch(err) {
-    res.status(500).json(err);
+      res.status(500).json(err);
   }
  
 });
@@ -51,20 +51,20 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try{ 
-    const categoryData = await Category.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
-    if (!categoryData[0]) {
-      res.status(404).json({message: 'No category with this id!'});
-      return;
-    }
-      res.status(200).json(categoryData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
+  const categoryData = await Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
   });
+  if (!categoryData[0]) {
+    res.status(404).json({message: 'No category with this id!'});
+    return;
+  }
+    res.status(200).json(categoryData);
+  } catch (err) {
+      res.status(500).json(err);
+  }
+});
 
 
 router.delete('/:id', async (req, res) => {
