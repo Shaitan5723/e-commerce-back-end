afcb425a9ea29f9bee191295a53002579b54b149
Shaitan5723 <105050107+Shaitan5723@users.noreply.github.com>
2022-08-15 18:20:56 +1000

    success message updated

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -60,7 +60,7 @@ router.put('/:id', async (req, res) => {
     res.status(404).json({message: 'No category with this id!'});
     return;
   }
-    res.status(200).json(categoryData);
+    res.status(200).json(`Category Updated`);
   } catch (err) {
       res.status(500).json(err);
   }
@@ -79,7 +79,7 @@ router.delete('/:id', async (req, res) => {
     res.status(400).json({message: 'No category with this id!'});
     return;
   }
-    res.status(200).json(categoryData);
+    res.status(200).json('Category Removed');
   } 
   catch(err) {
     res.status(500).json(err);
